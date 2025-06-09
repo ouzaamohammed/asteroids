b12cb5d86f8fff5e63ec25b9d25c5f81c4a56178
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-06-09 13:07:41 +0100

    render player & add rotation movement

--- main.py
+++ main.py
@@ -3,23 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        pygame.display.flip()
         dt = clock.tick(60) / 1000 # convert from milliseconds to seconds
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
