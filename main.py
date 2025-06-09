8ad56b493bd2367c551154b40bf5306ec71614a4
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-06-09 16:40:40 +0100

    use groups

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
     while True:
         for event in pygame.event.get():
@@ -19,8 +22,9 @@ def main():
                 return
         pygame.Surface.fill(screen, "black")
         dt = clock.tick(60) / 1000 # convert from milliseconds to seconds
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
